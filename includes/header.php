796776169d7bd6bb2f0b421b6677cf747941fe5b
Tori Bromma <toribromma@gmail.com>
2021-05-24 15:42:43 -0700

    redirected assets

--- includes/header.php
+++ includes/header.php
@@ -5,11 +5,11 @@
 <html>
 <head>
     <title><?php echo TITLE;?></title>
-    <link href="../assets/styles.css" rel="stylesheet">
+    <link href="./assets/styles.css" rel="stylesheet">
 <body id="final-example">
     <div class="wrapper">
         <div id="banner">
-            <a href="/" title="Return to Home">
+            <a href="index.php" title="Return to Home">
                 <img src="img/banner.png" alt="Franklin's Fine Dining">
             </a>
         </div><!--banner -->
